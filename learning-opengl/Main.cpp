bde8e6aa471e9f4fb28b6ab799edbdf149f04e11
Álex Lopez <33585530+alorodri@users.noreply.github.com>
2021-12-09 20:39:31 +0100

    pass array_buffer to gpu

--- learning-opengl/Main.cpp
+++ learning-opengl/Main.cpp
@@ -7,6 +7,7 @@
 
 void render();
 
+using uint = unsigned int;
 int main() {
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
@@ -47,4 +48,21 @@ int main() {
 void render()  {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        0.0f, 0.5f, 0.0f
+    };
+
+    // VERTEX INPUT START
+    uint VBO;
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    // STREAM_DRAW -> set only once and used at most a few times
+    // STATIC_DRAW -> set only once and used many times
+    // DYNAMIC_DRAW -> changed a lot and used many times
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // VERTEX INPUT END
 }
